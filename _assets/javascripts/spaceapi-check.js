e56603f832e80dd83e6ffb5e49855c0b54f97105
Jamie Reid <jamie@jre.id.au>
2017-06-03 17:04:53 +1000

    Clean up to a bare hugo scaffold.

--- _assets/javascripts/spaceapi-check.js
+++
@@ -1,8 +0,0 @@
-
-$( document ).ready(function() {
-    jQuery.get('/spaceapi.json',function(d){
-       $('#space-status-open').toggle(d.open);
-       $('#space-status-closed').toggle(!d.open);
-    });
-});
-
